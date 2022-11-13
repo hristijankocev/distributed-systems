e1517b4170feb95435abc906e3ae4737b02f1f2b
Hristijan Kocev <hristijankocev@ymail.com>
2022-11-13 15:42:26 +0100

    Server can now create ServerWorker threads to handle the request while the server can keep listening to new requests.

---
+++ src/mk/ukim/finki/server/Server.java
@@ -0,0 +1,47 @@
+package mk.ukim.finki.server;
+
+import mk.ukim.finki.config.ProtoConfig;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Server implements Runnable {
+    private final int port;
+    private final Hashtable<String, List<String>> users;
+
+    public Server(int port) {
+        this.port = port;
+        this.users = new Hashtable<>();
+    }
+
+    @Override
+    public void run() {
+        try {
+            DatagramSocket socket = new DatagramSocket(this.port);
+
+            System.out.println("Server started, listening on port " + this.port);
+
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                byte[] buffer = new byte[ProtoConfig.DATA.getUDP_PACKET_SIZE()];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                socket.receive(packet);
+
+                // Create a new thread to handle the request, so that we can keep listening to other requests
+                new Thread(new ServerWorker(this.users, socket, packet)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Server server = new Server(ProtoConfig.DATA.getSERVER_PORT());
+
+        new Thread(server).start();
+    }
+}
--- src/mk/ukim/finki/server/ServerWorker.java
+++ src/mk/ukim/finki/server/ServerWorker.java
@@ -9,116 +9,101 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.*;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
-public class ServerWorker extends Thread {
-    private final int port;
+public class ServerWorker implements Runnable {
     private final Hashtable<String, List<String>> users;
+    private final DatagramSocket socket;
+    private final DatagramPacket packet;
 
-    public ServerWorker(int port) {
-        this.port = port;
-        this.users = new Hashtable<>();
+    public ServerWorker(Hashtable<String, List<String>> users, DatagramSocket socket, DatagramPacket packet) {
+        this.users = users;
+        this.socket = socket;
+        this.packet = packet;
     }
 
     @Override
     public void run() {
-        try {
-            DatagramSocket ds = new DatagramSocket(this.port);
-
-            System.out.println("Server started, listening on port " + this.port);
-            //noinspection InfiniteLoopStatement
-            while (true) {
-                listen(ds);
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        handlePacket();
     }
 
-    private void listen(@NotNull DatagramSocket socket) throws IOException {
-        // Max UDP buffer size
-        var buffer = new byte[ProtoConfig.DATA.getUDP_PACKET_SIZE()];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-        // Thread is blocked until data is received
-        socket.receive(packet);
-        byte[] data = packet.getData();
-
-        String clientMessage = new String(data, packet.getOffset(), packet.getLength());
+    private synchronized void handlePacket() {
+        String clientMessage = new String(this.packet.getData(), this.packet.getOffset(), this.packet.getLength());
 
         // Print out the received message from the client
-        Date date = new Date();
-
-        System.out.print(date + " " + packet.getAddress() + " on port " + packet.getPort() + " said: ");
+        System.out.print(new Date() + " " + this.packet.getAddress() + " on port " + this.packet.getPort() + " said: ");
         System.out.println(clientMessage);
 
         // Handle the message
-        handleMessage(clientMessage, socket, packet);
+        handleMessage(clientMessage);
     }
 
-    private void handleMessage(@NotNull String clientMessage, @NotNull DatagramSocket socket, @NotNull DatagramPacket packet) {
+    private void handleMessage(@NotNull String clientMessage) {
         if (clientMessage.equals(ProtoConfig.DATA.getTEST_CONNECTION_MSG())) {
             // Hello message
-            sendMessage(packet, socket, "Welcome to the chat! " + ProtoConfig.DATA.getSERVER_HELP_MSG());
+            sendMessage("Welcome to the chat! " + ProtoConfig.DATA.getSERVER_HELP_MSG());
         } else if (clientMessage.equals("help")) {
             // Help message
-            sendMessage(packet, socket, ProtoConfig.DATA.getSERVER_COMMANDS());
+            sendMessage(ProtoConfig.DATA.getSERVER_COMMANDS());
         } else if (clientMessage.startsWith("login")) {
             // Validate the syntax for the login command
             String user = getUserFromLoginCommand(clientMessage);
             if (user == null) {
-                sendMessage(packet, socket, "Invalid login syntax. Expected 'login:\"username\"'");
+                sendMessage("Invalid login syntax. Expected 'login:\"username\"'");
             } else {
                 // Check if that username is already taken or if the user is already logged in
-                if (isLoggedIn(packet)) {
-                    Map.Entry<String, List<String>> existingUser = getUserBasedOnPacket(packet);
+                if (isLoggedIn()) {
+                    Map.Entry<String, List<String>> existingUser = getUserBasedOnPacket();
                     if (existingUser != null) {
-                        sendMessage(packet, socket, "You are already logged in as \"" + existingUser.getKey() + "\"!");
+                        sendMessage("You are already logged in as \"" + existingUser.getKey() + "\"!");
                     } else {
-                        sendMessage(packet, socket, "Username already taken!");
+                        sendMessage("Username already taken!");
                     }
                 } else {
                     if (this.users.keySet().stream().anyMatch(u -> u.equals(user))) {
-                        sendMessage(packet, socket, "Username already taken!");
+                        sendMessage("Username already taken!");
                     } else {
                         // Store the user in the hashtable
-                        this.users.put(user, List.of(packet.getAddress().getHostAddress(), String.valueOf(packet.getPort())));
-                        sendMessage(packet, socket, "Welcome back " + user + "! You are successfully logged in.");
+                        this.users.put(user, List.of(this.packet.getAddress().getHostAddress(), String.valueOf(this.packet.getPort())));
+                        sendMessage("Welcome back " + user + "! You are successfully logged in.");
                     }
                 }
             }
         } else if (clientMessage.equals("get-users")) {
             // In order to get the users, we first need to check if the user requesting the info is logged in
-            if (isLoggedIn(packet)) {
+            if (isLoggedIn()) {
                 String loggedInUsers = String.join(", ", this.users.keySet());
-                sendMessage(packet, socket, "Logged-in users: \n" + loggedInUsers);
+                sendMessage("Logged-in users: \n" + loggedInUsers);
             } else {
-                sendMessage(packet, socket, "You need to be logged in order to do that!");
+                sendMessage("You need to be logged in order to do that!");
             }
         } else if (clientMessage.startsWith("message")) {
-            if (isLoggedIn(packet)) {
-                sendMessageFromClientToClient(clientMessage, packet, socket);
+            if (isLoggedIn()) {
+                sendMessageFromClientToClient(clientMessage);
             } else {
-                sendMessage(packet, socket, "You need to be logged in order to do that!");
+                sendMessage("You need to be logged in order to do that!");
             }
         } else if (clientMessage.equals(ProtoConfig.DATA.getLOGOUT_MSG())) {
-            if (isLoggedIn(packet)) {
-                String user = getUserBasedOnPacket(packet).getKey();
+            if (isLoggedIn()) {
+                String user = getUserBasedOnPacket().getKey();
                 if (user != null) {
                     this.users.remove(user);
-                    sendMessage(packet, socket, "Logged out successfully.");
+                    sendMessage("Logged out successfully.");
                 }
             } else {
-                sendMessage(packet, socket, "Can't logout when you haven't even been logged in.");
+                sendMessage("Can't logout when you haven't even been logged in.");
             }
         } else {
             // The server doesn't understand the message
-            sendMessage(packet, socket, "Unknown command. " + ProtoConfig.DATA.getSERVER_COMMANDS());
+            sendMessage("Unknown command. " + ProtoConfig.DATA.getSERVER_COMMANDS());
         }
 
     }
 
-    private void sendMessageFromClientToClient(String packetContent, DatagramPacket packet, DatagramSocket socket) {
+    private void sendMessageFromClientToClient(String packetContent) {
         String[] command = packetContent.split(":");
 
         // Validate the command
@@ -128,7 +113,7 @@ public class ServerWorker extends Thread {
             if (this.users.containsKey(toClient)) {
                 List<String> userToSendTo = this.users.get(toClient);
                 try {
-                    Map.Entry<String, List<String>> user = getUserBasedOnPacket(packet);
+                    Map.Entry<String, List<String>> user = getUserBasedOnPacket();
                     String transitiveMessage = "You got a message from '" + user.getKey() + "' saying: " + command[2];
                     byte[] transitiveMessageBytes = transitiveMessage.getBytes();
 
@@ -137,36 +122,35 @@ public class ServerWorker extends Thread {
 
                     DatagramPacket dp = new DatagramPacket(transitiveMessageBytes, transitiveMessageBytes.length, outgoingAddress, outgoingPort);
 
-                    socket.send(dp);
+                    this.socket.send(dp);
 
                     // Tell the original client that the message was sent successfully
-                    String message = "Your message was sent successfully!";
-                    sendMessage(packet, socket, message);
+                    sendMessage("Your message was sent successfully!");
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
-                    sendMessage(packet, socket, "Error occurred while trying to send a message to " + toClient);
+                    sendMessage("Error occurred while trying to send a message to " + toClient);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else {
-                sendMessage(packet, socket, "Client " + toClient + " does not exist in the logged in list.");
+                sendMessage("Client " + toClient + " does not exist in the logged in list.");
             }
         } else {
-            sendMessage(packet, socket, "Invalid message sending syntax. Expected 'message:toUser:messageContent'");
+            sendMessage("Invalid message sending syntax. Expected 'message:toUser:messageContent'");
         }
     }
 
-    private Map.Entry<String, List<String>> getUserBasedOnPacket(DatagramPacket packet) {
+    private Map.Entry<String, List<String>> getUserBasedOnPacket() {
         return this.users.entrySet().stream()
-                .filter(e -> e.getValue().get(0).equals(packet.getAddress().getHostAddress())
-                        && e.getValue().get(1).equals(String.valueOf(packet.getPort())))
+                .filter(e -> e.getValue().get(0).equals(this.packet.getAddress().getHostAddress())
+                        && e.getValue().get(1).equals(String.valueOf(this.packet.getPort())))
                 .findFirst()
                 .orElse(null);
     }
 
-    private boolean isLoggedIn(@NotNull DatagramPacket packet) {
-        String clientAddress = packet.getAddress().getHostAddress();
-        String clientPort = String.valueOf(packet.getPort());
+    private boolean isLoggedIn() {
+        String clientAddress = this.packet.getAddress().getHostAddress();
+        String clientPort = String.valueOf(this.packet.getPort());
 
         return this.users.values()
                 .stream()
@@ -183,24 +167,15 @@ public class ServerWorker extends Thread {
         return null;
     }
 
-    private void sendMessage(@NotNull DatagramPacket packet, @NotNull DatagramSocket socket, @NotNull String message) {
+    private void sendMessage(@NotNull String message) {
         try {
-
             byte[] messageBytes = message.getBytes();
 
-            DatagramPacket dp = new DatagramPacket(messageBytes, messageBytes.length, packet.getAddress(), packet.getPort());
+            DatagramPacket dp = new DatagramPacket(messageBytes, messageBytes.length, this.packet.getAddress(), this.packet.getPort());
 
-            socket.send(dp);
+            this.socket.send(dp);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) {
-        int port = ProtoConfig.DATA.getSERVER_PORT();
-
-        ServerWorker serverWorker = new ServerWorker(port);
-        serverWorker.start();
-    }
 }
-
